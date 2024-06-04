d909db712249f87f60e7dbfd1a0b4092c0e46a03
mahmedk123 <moh_rulz@hotmail.co.uk>
2024-06-04 22:31:06 +0100

    merge code

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import Nav from '../src/components/Nav';
 import Layout from '../src/components/Layout';
@@ -6,11 +6,6 @@ import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../src/styles/theme.js';
 
 export default function Home() {
-  useEffect(() => {
-    const header = document.querySelector('.header h1');
-    console.log('Header font family:', window.getComputedStyle(header).fontFamily);
-  }, []);
-
   return (
     <ChakraProvider theme={theme}>
       <Layout>
@@ -36,105 +31,140 @@ export default function Home() {
                 <p className="chicken-shaq">BY THE CHICKEN SHAQ</p>
                 <img src="/milkshake.svg" alt="Milkshake Icon" className="milkshake-icon" />
               </div>
+              <div className="uber-eats-container">
+                <p>NOW AVAILABLE ON UBER EATS</p>
+                <a href="https://www.ubereats.com/gb/store/flippin-burgers-%26-shakes/xk_u5JpzReG7ddvZTTX0bQ" target="_blank" rel="noopener noreferrer" className="uber-eats-link">
+                  <img src="/uber-eats.svg" alt="Uber Eats Icon" className="uber-eats-icon" />
+                </a>
+              </div>
             </div>
-
-            {/* Uber Eats */}
-            <a href="https://www.ubereats.com/gb/store/flippin-burgers-%26-shakes/xk_u5JpzReG7ddvZTTX0bQ" target="_blank" rel="noopener noreferrer" className="uber-eats-link">
-              <img src="/uber-eats.svg" alt="Uber Eats" className="uber-eats-icon" />
-            </a>
           </main>
           {/* Footer */}
           <footer className="footer">
             <div className="border-line"></div>
-            <p>Telephone: 01234567865</p>
+            <p>Telephone: 01204 665665</p>
           </footer>
         </div>
         <style jsx>{`
-          .content {
-            display: flex;
-            flex-direction: column;
-            min-height: 100vh; /* Set the minimum height of the content to be at least the height of the viewport */
-          }
-
-          .main {
-            flex: 1; /* Allow the main content to grow to fill the remaining space */
-            text-align: center;
-            padding: 50px;
-            background: transparent;
-            color: #fff;
-          }
-
-          .footer {
-            text-align: center;
-            font-family: 'Montserrat', sans-serif; /* Use body font from theme */
-            font-size: 2em;
-            background-color: rgba(0, 0, 0, 0.5); /* Add a background color for better visibility */
-          }
-
-          .header h1 {
-            font-family: 'Montserrat', sans-serif;
-            font-size: 2em;
-            color: #fff;
-            text-align: center;
-            margin-top: 20px;
-          }
-
-          .border-line {
-            width: 80%;
-            margin: 10px auto;
-            border-bottom: 2px solid #fff;
-          }
-
-          .logotest-container {
-            display: flex;
-            justify-content: center;
-            margin-bottom: 20px;
-          }
-
+         .content {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh; /* Set the minimum height of the content to be at least the height of the viewport */
+        }
+        
+        .main {
+          flex: 1; /* Allow the main content to grow to fill the remaining space */
+          text-align: center;
+          padding: 50px;
+          background: transparent;
+          color: #fff;
+        }
+        
+        .footer {
+          text-align: center;
+          font-family: 'Montserrat', sans-serif; /* Use body font from theme */
+          font-size: 2em;
+          background-color: rgba(0, 0, 0, 0.5); /* Add a background color for better visibility */
+        }
+        
+        .header h1 {
+          font-family: 'Montserrat', sans-serif;
+          font-size: 2em;
+          color: #fff;
+          text-align: center;
+          margin-top: 20px;
+        }
+        
+        .border-line {
+          width: 80%;
+          margin: 10px auto;
+          border-bottom: 2px solid #fff;
+        }
+        
+        .logotest-container {
+          display: flex;
+          justify-content: center;
+          margin-bottom: 20px;
+        }
+        
+        .logotest {
+          height: 22em;
+        }
+        
+        .icons {
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: center;
+          margin-top: 20px;
+        }
+        
+        .icon-wrapper {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-top: 10px;
+        }
+        
+        .burger-icon {
+          height: 4em;
+          margin-right: 10px;
+        }
+        
+        .milkshake-icon {
+          height: 4em;
+          margin-left: 10px;
+        }
+        
+        .chicken-shaq {
+          font-family: 'Pacifico', cursive;
+          color: orange;
+          font-size: 2.5em;
+          margin: 0;
+        }
+        
+        .uber-eats-container {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          margin-top: 20px; /* Adjust margin as needed */
+          font-family: 'Montserrat', sans-serif;
+          font-size: 2.5m;
+        }
+        
+        .uber-eats-link {
+          display: block; /* Make the link a block element to occupy full width */
+          margin-left: 10px; /* Add margin to create space between text and icon */
+        }
+        
+        .uber-eats-icon {
+          height: 6em;
+        }
+        
+        /* Media query for mobile screens */
+        @media (max-width: 768px) {
           .logotest {
-            height: 22em;
+            height: 12em; /* Adjust height for smaller screens */
           }
-
+        
           .icons {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-            margin-top: 20px;
+            flex-direction: row; /* Change flex-direction to row for smaller screens */
           }
-
+        
           .icon-wrapper {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            margin-top: 10px;
+            margin-top: 0; /* Remove top margin for smaller screens */
           }
-
+        
           .burger-icon {
-            height: 4em;
-            margin-right: 10px;
+            position: fixed; /* Position burger icon fixed for mobile */
+            top: 10px; /* Adjust top position as needed */
+            left: 10px; /* Adjust left position as needed */
           }
+        }
 
-          .milkshake-icon {
-            height: 4em;
-            margin-left: 10px;
-          }
-
-          .chicken-shaq {
-            font-family: 'Pacifico', cursive;
-            color: orange;
-            font-size: 2.5em;
-            margin: 0;
-          }
-
-          .uber-eats-link {
-            margin-top: 20px; /* Adjust margin as needed */
-            display: block; /* Make the link a block element to occupy full width */
-          }
-
-          .uber-eats-icon {
-            height: 4em;
-           
+        /* Blur effect */
+        .blur {
+          filter: blur(3px); /* Adjust the blur intensity as needed */
         }
         
         `}</style>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,32 @@
-// src/components/Nav.js
 import React, { useState } from 'react';
 import Link from 'next/link';
 import { Flex, Box, Button, VStack } from '@chakra-ui/react';
 import {
   IconHome2,
-  IconGauge,
-  IconDeviceDesktopAnalytics,
-  IconCalendarStats,
+  IconBurger,
+  IconAddressBook,
+  IconMenu2, // Import the burger icon
 } from '@tabler/icons-react';
-import MyLogo from '../styles/flippin.png';
 import { SignedIn, SignedOut, UserButton, useClerk } from '@clerk/nextjs';
 
 const Nav = () => {
-  const [active, setActive] = useState(0);
+  const [isNavOpen, setIsNavOpen] = useState(false); // State to track if nav is open
   const { signOut } = useClerk();
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen); // Toggle the state of the nav
+    
+    // Add or remove blur class from content element
+    const content = document.querySelector('.content');
+    if (content) {
+      content.classList.toggle('blur');
+    }
+  };
 
   const mockdata = [
     { icon: IconHome2, label: 'Home', href: '/' },
-    { icon: IconGauge, label: 'About', href: '/about' },
-    { icon: IconDeviceDesktopAnalytics, label: 'Menu', href: '/menu' },
-    { icon: IconCalendarStats, label: 'Contact', href: '/contact' },
+    { icon: IconBurger, label: 'Menu', href: '/menu' },
+    { icon: IconAddressBook, label: 'Contact', href: '/contact' },
   ];
 
   const links = mockdata.map((link, index) => {
@@ -36,7 +43,6 @@ const Nav = () => {
         textDecoration="none"
         _hover={{ bg: 'orange.500' }}
         _active={{ bg: 'gray.800' }}
-        onClick={() => setActive(index)}
         fontFamily="'Roboto', sans-serif"
         fontSize="1.2em"
         mt="1rem"
@@ -48,58 +54,46 @@ const Nav = () => {
   });
 
   return (
-    <Flex
-      direction="column"
-      width={{ base: 300 }}
-      p="xs"
-      bg="#000"
-      h="100vh"
-      pos="fixed"
-      top={0}
-      left={0}
-      justifyContent="space-between"
-    >
-      <Box>
-        {/* <img src={MyLogo} alt="Your Logo" style={{ width: '30px', height: '30px' }} /> */}
-        <VStack spacing="4" mt="4">
-          {links}
-        </VStack>
-      </Box>
-      <Box mt="auto" mb="4">
-        <SignedOut>
-          <Button
-            as={Link}
-            href="/signin"
-            color="white"
-            size="lg"
-            variant="solid"
-            bg="orange.500"
-            _hover={{ bg: 'orange.600' }}
-            fontFamily="'Roboto', sans-serif"
-            fontSize="1.2em"
-            mt="1rem"
-          >
-            Sign In
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <VStack spacing="4">
-            <UserButton
-              afterSignOutUrl="/"
-              appearance={{
-                elements: {
-                  button: {
-                    size: 'lg',
-                    bg: 'orange.500',
-                    color: 'white',
-                    _hover: { bg: 'orange.600' },
-                    fontFamily: "'Roboto', sans-serif",
-                    fontSize: '1.2em',
-                  },
-                },
-              }}
-            />
+    <>
+      {/* Burger icon */}
+      <Button
+        className="burger-icon"
+        onClick={toggleNav}
+        variant="unstyled"
+        color="white"
+        size="lg"
+        pos="fixed"
+        top={0}
+        left={0}
+        zIndex={999}
+        m="1rem"
+      >
+        <IconMenu2 />
+      </Button>
+      <Flex
+        direction="column"
+        width={{ base: 300 }}
+        p="xs"
+        bg="#000"
+        h="100vh"
+        pos="fixed"
+        top={0}
+        left={isNavOpen ? 0 : '-300px'} // Slide the nav off-screen when closed
+        transition="left 0.3s ease" // Add transition for smooth animation
+        justifyContent="space-between"
+        className="nav"
+        zIndex={998} // Ensure the nav is behind the burger icon
+      >
+        <Box>
+          <VStack spacing="4" mt="4">
+            {links}
+          </VStack>
+        </Box>
+        <Box mb="4">
+          <SignedOut>
             <Button
+              as={Link}
+              href="/signin"
               color="white"
               size="lg"
               variant="solid"
@@ -108,14 +102,45 @@ const Nav = () => {
               fontFamily="'Roboto', sans-serif"
               fontSize="1.2em"
               mt="1rem"
-              onClick={() => signOut()}
             >
-              Sign Out
+              Sign In
             </Button>
-          </VStack>
-        </SignedIn>
-      </Box>
-    </Flex>
+          </SignedOut>
+          <SignedIn>
+            <VStack spacing="4">
+              <UserButton
+                afterSignOutUrl="/"
+                appearance={{
+                  elements: {
+                    button: {
+                      size: 'lg',
+                      bg: 'orange.500',
+                      color: 'white',
+                      _hover: { bg: 'orange.600' },
+                      fontFamily: "'Roboto', sans-serif",
+                      fontSize: '1.2em',
+                    },
+                  },
+                }}
+              />
+              <Button
+                color="white"
+                size="lg"
+                variant="solid"
+                bg="orange.500"
+                _hover={{ bg: 'orange.600' }}
+                fontFamily="'Roboto', sans-serif"
+                fontSize="1.2em"
+                mt="1rem"
+                onClick={() => signOut()}
+              >
+                Sign Out
+              </Button>
+            </VStack>
+          </SignedIn>
+        </Box>
+      </Flex>
+    </>
   );
 };
 
