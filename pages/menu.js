44e22dfecbd26a36b94da3186d4fa5a25e92007d
mahmedk123 <moh_rulz@hotmail.co.uk>
2024-06-08 12:14:55 +0100

    refactor code

--- pages/menu.js
+++ pages/menu.js
@@ -5,7 +5,6 @@ import {
   Box,
   Button,
   Container,
-  Collapse,
   Flex,
   FormControl,
   FormLabel,
@@ -15,8 +14,9 @@ import {
   VStack,
   Icon,
   Text,
+  HStack,
+  useBreakpointValue,
 } from '@chakra-ui/react';
-import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons';
 
 const categories = [
   { label: 'Smash Burgers', type: 'smashburgers' },
@@ -37,17 +37,20 @@ export async function getStaticProps() {
         throw new Error(`Failed to fetch menu data for ${type}`);
       }
       const data = await res.json();
-      return data;
+      return data.length > 0 ? data : null; // Return null if no data
     } catch (error) {
       console.error(`Error fetching menu data for ${type}:`, error);
-      return [];
+      return null; // Return null in case of error
     }
   };
 
   const menuItems = {};
   for (const category of categories) {
     try {
-      menuItems[category.type] = await fetchMenuData(category.type);
+      const data = await fetchMenuData(category.type);
+      if (data) {
+        menuItems[category.type] = data;
+      }
     } catch (error) {
       console.error(`Error fetching data for category ${category.type}:`, error);
       menuItems[category.type] = [];
@@ -58,35 +61,14 @@ export async function getStaticProps() {
     props: {
       initialMenuItems: menuItems,
     },
-    revalidate: 10, // If you want to enable Incremental Static Regeneration
+    revalidate: 10, // Enable Incremental Static Regeneration
   };
 }
 
 const MenuPage = ({ initialMenuItems }) => {
   const [menuItemsCache, setMenuItemsCache] = useState(initialMenuItems);
-  const [isOpen, setIsOpen] = useState(
-    categories.reduce((acc, category) => {
-      acc[category.type] = false;
-      return acc;
-    }, {})
-  );
-  const [fetchStatus, setFetchStatus] = useState(
-    categories.reduce((acc, category) => {
-      acc[category.type] = initialMenuItems[category.type].length > 0;
-      return acc;
-    }, {})
-  );
-
   const { isSignedIn } = useUser();
   const [formData, setFormData] = useState({ name: '', price: '', description: '' });
-  const [selectedLabel, setSelectedLabel] = useState(null); // Track selected label for animation
-
-  const toggleCollapse = (type) => {
-    setIsOpen((prev) => ({
-      ...prev,
-      [type]: !prev[type],
-    }));
-  };
 
   const fetchMenuData = async (type) => {
     try {
@@ -95,24 +77,22 @@ const MenuPage = ({ initialMenuItems }) => {
         throw new Error('Failed to fetch menu data');
       }
       const data = await res.json();
-      setMenuItemsCache((prev) => ({ ...prev, [type]: data }));
-      if (data.length === 0) {
-        setFetchStatus((prev) => ({ ...prev, [type]: false }));
+      if (data.length > 0) {
+        setMenuItemsCache((prev) => ({ ...prev, [type]: data }));
       }
     } catch (error) {
       console.error(`Error fetching menu data for ${type}:`, error);
       setMenuItemsCache((prev) => ({ ...prev, [type]: [] }));
-      setFetchStatus((prev) => ({ ...prev, [type]: false }));
     }
   };
 
   useEffect(() => {
     categories.forEach(({ type }) => {
-      if (menuItemsCache[type].length === 0 && fetchStatus[type]) {
+      if (!menuItemsCache[type] || menuItemsCache[type].length === 0) {
         fetchMenuData(type);
       }
     });
-  }, [menuItemsCache, fetchStatus]);
+  }, [menuItemsCache]);
 
   const handleSubmit = async (e, type) => {
     e.preventDefault();
@@ -129,18 +109,13 @@ const MenuPage = ({ initialMenuItems }) => {
         },
         body: JSON.stringify(newItem),
       });
-  
+
       if (!res.ok) {
         throw new Error(`Failed to add new menu item: ${res.statusText}`);
       }
-  
-      // Read the response body once
-      const responseData = await res.clone().json();
-  
-      // Handle the response data
-      // console.log(responseData);
-  
-      // Update state or perform any other actions as needed
+
+      const responseData = await res.json();
+
       setMenuItemsCache((prev) => ({
         ...prev,
         [type]: [...prev[type], responseData],
@@ -151,98 +126,112 @@ const MenuPage = ({ initialMenuItems }) => {
     }
   };
 
+  const isMobile = useBreakpointValue({ base: true, md: false });
+
   return (
     <Container maxW="container.xl" minH="100vh" position="relative" pb="40px">
       <Nav />
       <Heading as="h1" textAlign="center" my="8">
         Menu
       </Heading>
-      {categories.map(({ label, type }) => (
-        <Box key={type} my="12">
-          <Flex
-            justify="space-between"
-            align="center"
-            borderBottom="2px"
-            borderColor="orange.500"
-            pb="4"
-            cursor="pointer"
-            onClick={() => toggleCollapse(type)}
-            direction={{ base: 'column', md: 'row' }} // Adjust direction based on screen size
-            transition={`all 0.1s ${isOpen[type] ? '' : 'ease-in-out'}`} // Adjust easing based on collapse state
-            bg={selectedLabel === type ? 'orange.100' : 'transparent'} // Add selected label effect
+      <HStack
+        overflowX="auto"
+        spacing={4}
+        pb={4}
+        mb={4}
+        borderBottom="2px"
+        borderColor="orange.500"
+        position="sticky" // Make the buttons sticky
+        top="0"
+        bg="white" // Ensure background color for better visibility
+        zIndex="10" // Ensure the buttons are above other content
+        css={{
+          '&::-webkit-scrollbar': { display: 'none' },
+          msOverflowStyle: 'none',
+          scrollbarWidth: 'none',
+        }}
+      >
+        {categories.map(({ label, type }) => (
+          <Button
+            key={type}
+            onClick={() => document.getElementById(type).scrollIntoView({ behavior: 'smooth' })}
+            _hover={{ bg: 'orange.500' }}
+            flexShrink={0}
           >
-            <Heading as="h2" size="lg" mb={{ base: '4', md: '0' }}> {/* Adjust margin bottom based on screen size */}
-              {label}
-            </Heading>
-            <Icon as={isOpen[type] ? ChevronUpIcon : ChevronDownIcon} w={6} h={6} />
-          </Flex>
-          <Collapse in={isOpen[type]}>
-            <VStack spacing="4" align="start" mt="4">
-              {(menuItemsCache[type] || []).map((item, index) => (
-                <Box
-                  key={index}
-                  p="2"
-                  borderWidth="3px"
-                  borderRadius="xl"
-                  w={{ base: '100%', md: '75%', lg: '50%' }}
-                  margin="auto"
-                  mt="4"
-                  boxShadow="md"
-                  textAlign="center"
-                >
-                  <Text fontWeight="bold" fontSize="xl">
-                    {item.foodname}
-                  </Text>
-                  <Box mt="2">
-                    <Text>{item.description}</Text>
-                  </Box>
-                  <Flex justifyContent="center" alignItems="center" mt="2">
-                    <Text alignSelf="flex-start">{item.description ? 'on its own' : ''}</Text>
-                    <Box ml="2">£{item.foodprice}</Box>
-                  </Flex>
+            {label}
+          </Button>
+        ))}
+      </HStack>
+      {categories.map(({ label, type }) => (
+        <Box key={type} my="12" id={type}>
+          <Heading as="h2" size="lg" mb="4">
+            {label}
+          </Heading>
+          <VStack spacing="4" align="start">
+            {(menuItemsCache[type] || []).map((item, index) => (
+              <Box
+                key={index}
+                p="2"
+                borderWidth="3px"
+                borderRadius="xl"
+                w={{ base: '100%', md: '75%', lg: '50%' }}
+                margin="auto"
+                mt="4"
+                boxShadow="md"
+                textAlign="center"
+              >
+                <Text fontWeight="bold" fontSize="xl">
+                  {item.foodname}
+                </Text>
+                <Box mt="2">
+                  <Text>{item.description}</Text>
                 </Box>
-              ))}
-            </VStack>
-            {isSignedIn && (
-              <Box as="form" onSubmit={(e) => handleSubmit(e, type)} mt="4">
-                <FormControl mb="4">
-                  <FormLabel>Name:</FormLabel>
-                  <Input
-                    type="text"
-                    name="name"
-                    placeholder="Enter item name"
-                    value={formData.name}
-                    onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                    required
-                  />
-                </FormControl>
-                <FormControl mb="4">
-                  <FormLabel>Price:</FormLabel>
-                  <Input
-                    type="text"
-                    name="price"
-                    placeholder="Enter item price"
-                    value={formData.price}
-                    onChange={(e) => setFormData({ ...formData, price: e.target.value })}
-                    required
-                  />
-                </FormControl>
-                <FormControl mb="4">
-                  <FormLabel>Description:</FormLabel>
-                  <Textarea
-                    name="description"
-                    placeholder="Enter item description"
-                    value={formData.description}
-                    onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-                    required
-                  />
-                </FormControl>
-                <Button type="submit" colorScheme="orange">
-                  Add {label}
-                </Button>
+                <Flex justifyContent="center" alignItems="center" mt="2">
+                  <Text alignSelf="flex-start">{item.description ? 'on its own' : ''}</Text>
+                  <Box ml="2">£{item.foodprice}</Box>
+                </Flex>
               </Box>
-            )}
-          </Collapse>
+            ))}
+          </VStack>
+          {isSignedIn && (
+            <Box as="form" onSubmit={(e) => handleSubmit(e, type)} mt="4">
+              <FormControl mb="4">
+                <FormLabel>Name:</FormLabel>
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Enter item name"
+                  value={formData.name}
+                  onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel>Price:</FormLabel>
+                <Input
+                  type="text"
+                  name="price"
+                  placeholder="Enter item price"
+                  value={formData.price}
+                  onChange={(e) => setFormData({ ...formData, price: e.target.value })}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel>Description:</FormLabel>
+                <Textarea
+                  name="description"
+                  placeholder="Enter item description"
+                  value={formData.description}
+                  onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+                  required
+                />
+              </FormControl>
+              <Button type="submit" colorScheme="orange">
+                Add {label}
+              </Button>
+            </Box>
+          )}
         </Box>
       ))}
       <Box
