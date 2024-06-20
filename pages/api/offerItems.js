1aa7e1ae269d0b7ad9ae48e00a7ea5c524abacd9
mahmedk123 <moh_rulz@hotmail.co.uk>
2024-06-20 17:39:09 +0100

    deploy to work

--- pages/api/offerItems.js
+++ pages/api/offerItems.js
@@ -2,19 +2,18 @@ import { sql } from '@vercel/postgres';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Handle POST requests for creating a new menu item
+    // Handle POST requests for creating a new offer item
     const { name, price, description, imageURL, type } = req.body;
-    
 
     // Check if required fields are provided
     if (!name || !price || !description || !type) {
       return res.status(400).json({ error: 'Name, price, description, type are required' });
     }
-    
-   
+
     try {
       let result;
 
+      // Depending on whether imageURL is provided, insert into the database
       if (imageURL) {
         result = await sql`
           INSERT INTO offertest (foodName, foodPrice, foodType, description, imageUrl) 
@@ -31,36 +30,36 @@ export default async function handler(req, res) {
 
       res.status(201).json(result.rows[0]);
     } catch (error) {
-      console.error('Error creating item:', error);
+      console.error('Error creating offer item:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  }else if (req.method === 'GET') {
-    // Handle GET requests for fetching menu items
+  } else if (req.method === 'GET') {
+    // Handle GET requests for fetching offer items
     const { type } = req.query;
-  
+
     // Check if required query parameter is provided
     if (!type) {
       return res.status(400).json({ error: 'Type is required' });
     }
-  
+
     try {
       const result = await sql`
         SELECT foodName, description, foodPrice FROM offertest WHERE foodType = ${type}
       `;
       res.status(200).json(result.rows);
     } catch (error) {
-      console.error('Error fetching menu items:', error);
+      console.error('Error fetching offer items:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
-  }  else if (req.method === 'DELETE') {
-    // Handle DELETE requests for deleting a menu item
+  } else if (req.method === 'DELETE') {
+    // Handle DELETE requests for deleting an offer item
     const { name } = req.query;
-  
+
     // Check if required query parameter is provided
     if (!name) {
       return res.status(400).json({ error: 'Name is required' });
-    } 
-  
+    }
+
     try {
       const result = await sql`
         DELETE FROM offertest WHERE foodName = ${name}
@@ -68,8 +67,8 @@ export default async function handler(req, res) {
       `;
       res.status(200).json(result.rows);
     } catch (error) {
-      console.error('Error deleting menu item:', error);
+      console.error('Error deleting offer item:', error);
       res.status(500).json({ error: 'Internal Server Error' });
-    } 
-}
+    }
+  }
 }
--- pages/offers.js
+++ pages/offers.js
@@ -1,99 +1,130 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import Nav from '../src/components/Nav';
 import { useUser } from '@clerk/nextjs';
 import {
   Box,
   Button,
   Container,
+  Flex,
   FormControl,
   FormLabel,
   Heading,
   Input,
   Textarea,
   VStack,
-  Flex,
   Text,
-  Image,
   HStack,
+  useBreakpointValue,
+  Image,
 } from '@chakra-ui/react';
-import Nav from '../src/components/Nav';
 
-const categories = [{ foodType: 'offers' }];
+const categories = [
+  { label: 'Offers', type: 'offers' }, // Adjusted categories for offers
+];
 
 export async function getStaticProps() {
-  try {
-    const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
-    const res = await fetch(`${baseUrl}/api/offerItems?type=offers`);
-    if (!res.ok) {
-      throw new Error(`Failed to fetch offer data for offers - ${res.status} ${res.statusText}`);
+  const fetchOfferData = async (type) => {
+    try {
+      const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+      const res = await fetch(`${baseUrl}/api/offerItems?type=${type}`);
+      if (!res.ok) {
+        throw new Error(`Failed to fetch offer data for ${type}`);
+      }
+      const data = await res.json();
+      return data.length > 0 ? data : null;
+    } catch (error) {
+      console.error(`Error fetching offer data for ${type}:`, error);
+      return null;
     }
-    const data = await res.json();
-    const initialOfferItems = data.length > 0 ? { offers: data } : {};
-    
-    return {
-      props: {
-        initialOfferItems,
-      },
-      revalidate: 10,
-    };
-  } catch (error) {
-    console.error(`Error fetching offer data for offers:`, error);
-    return {
-      props: {
-        initialOfferItems: {}, // Return empty initial data or handle fallback gracefully
-      },
-      revalidate: 10,
-    };
+  };
+
+  const offerItems = {};
+  for (const category of categories) {
+    try {
+      const data = await fetchOfferData(category.type);
+      if (data) {
+        offerItems[category.type] = data;
+      }
+    } catch (error) {
+      console.error(`Error fetching data for category ${category.type}:`, error);
+      offerItems[category.type] = [];
+    }
   }
+
+  return {
+    props: {
+      initialOfferItems: offerItems,
+    },
+    revalidate: 10,
+  };
 }
 
 const OffersPage = ({ initialOfferItems }) => {
-  const { isSignedIn } = useUser();
   const [offerItemsCache, setOfferItemsCache] = useState(initialOfferItems);
+  const { isSignedIn } = useUser();
   const [formData, setFormData] = useState({ name: '', price: '', description: '', imageURL: '' });
 
-  const handleDelete = async (foodType, foodname) => {
-    const encodedName = encodeURIComponent(foodname);
+  const handleDelete = async (type, name) => {
+    const encodedName = encodeURIComponent(name);
     const isConfirmed = window.confirm('Are you sure you want to delete this item?');
     if (!isConfirmed) {
       return;
     }
-  
     try {
-      const res = await fetch(`/api/offerItems?type=${foodType}&name=${encodedName}`, {
+      const res = await fetch(`/api/offerItems?type=${type}&name=${encodedName}`, {
         method: 'DELETE',
       });
       if (!res.ok) {
         throw new Error('Failed to delete offer item');
       }
       console.log('Offer item deleted successfully');
-      
-      // Update offerItemsCache immediately after successful deletion
       setOfferItemsCache((prev) => {
-        const filteredItems = prev[foodType].filter(item => item.foodname !== foodname); // Use foodname here
+        const filteredItems = prev[type].filter(item => item.foodname !== name);
         return {
           ...prev,
-          [foodType]: filteredItems,
+          [type]: filteredItems,
         };
       });
     } catch (error) {
       console.error('Error deleting offer item:', error);
     }
   };
-  
+
+  const fetchOfferData = async (type) => {
+    try {
+      const res = await fetch(`/api/offerItems?type=${type}`);
+      if (!res.ok) {
+        throw new Error('Failed to fetch offer data');
+      }
+      const data = await res.json();
+      if (data.length > 0) {
+        setOfferItemsCache((prev) => ({ ...prev, [type]: data }));
+      }
+    } catch (error) {
+      console.error(`Error fetching offer data for ${type}:`, error);
+      setOfferItemsCache((prev) => ({ ...prev, [type]: [] }));
+    }
+  };
+
+  useEffect(() => {
+    categories.forEach(({ type }) => {
+      if (!offerItemsCache[type] || offerItemsCache[type].length === 0) {
+        fetchOfferData(type);
+      }
+    });
+  }, [offerItemsCache]);
 
   const handleSubmit = async (e, type) => {
     e.preventDefault();
-
+  
     const newItem = {
       name: formData.name,
-      price: formData.price,
+      price: parseFloat(formData.price), // Ensure price is sent as number or string
       description: formData.description,
       imageURL: formData.imageURL,
-      type: type  // Include the type field
+      type: type, // Ensure type is correctly passed
     };
-
-    console.log('Submitting new item:', newItem);  // Log the data
-
+  
     try {
       const res = await fetch(`/api/offerItems`, {
         method: 'POST',
@@ -102,10 +133,11 @@ const OffersPage = ({ initialOfferItems }) => {
         },
         body: JSON.stringify(newItem),
       });
+  
       if (!res.ok) {
-        const errorText = await res.text();  // Capture error text
-        throw new Error(`Failed to add new offer item: ${res.statusText} - ${errorText}`);
+        throw new Error(`Failed to add new offer item: ${res.statusText}`);
       }
+  
       const responseData = await res.json();
       setOfferItemsCache((prev) => ({
         ...prev,
@@ -116,32 +148,33 @@ const OffersPage = ({ initialOfferItems }) => {
       console.error('Error adding new offer item:', error);
     }
   };
+  
 
   const handleImageUpload = async (event) => {
     if (!event.target.files || event.target.files.length === 0) {
       console.error('No file selected.');
       return;
     }
-  
+
     const file = event.target.files[0];
     const formData = new FormData();
     formData.append('image', file);
-  
+
     try {
       const response = await fetch('/api/uploadImage', {
         method: 'POST',
         body: formData,
       });
-  
+
       if (!response.ok) {
         const errorText = await response.text();
         throw new Error(`Image upload failed: ${response.status} ${response.statusText} - ${errorText}`);
       }
-  
+
       const data = await response.json();
-  
+
       if (data.url) {
-        setFormData((prevFormData) => ({ ...prevFormData, imageURL: data.url })); // Update imageURL in state
+        setFormData((prevFormData) => ({ ...prevFormData, imageURL: data.url }));
         console.log("Image URL set to:", data.url);
       } else {
         console.error('Received data:', data);
@@ -150,19 +183,15 @@ const OffersPage = ({ initialOfferItems }) => {
       console.error('Error uploading image:', error);
     }
   };
-  
+
+  const isMobile = useBreakpointValue({ base: true, md: false });
 
   return (
     <Container maxW="container.xl" minH="100vh" position="relative" pb="40px">
       <Nav />
-      <title>Offers only - limited time</title>
-      <meta name="description" content="Check out our limited-time offers!" />
-      <link rel="icon" href="/favicon.ico" />
-
       <Heading as="h1" textAlign="center" my="8">
         Offers
       </Heading>
-
       <HStack
         overflowX="auto"
         spacing={4}
@@ -176,119 +205,136 @@ const OffersPage = ({ initialOfferItems }) => {
           scrollbarWidth: 'none',
         }}
       >
-        {categories.map(({ foodType }) => (
+        {categories.map(({ label, type }) => (
           <Button
-            key={foodType}
-            onClick={() => document.getElementById(foodType).scrollIntoView({ behavior: 'smooth' })}
+            key={type}
+            onClick={() => document.getElementById(type).scrollIntoView({ behavior: 'smooth' })}
             _hover={{ bg: 'orange.500' }}
             flexShrink={0}
           >
-            {foodType}
+            {label}
           </Button>
         ))}
       </HStack>
-
-      {categories.map(({ foodType }) => (
-        <Box key={foodType} my="12" id={foodType}>
+      {categories.map(({ label, type }) => (
+        <Box key={type} my="12" id={type}>
           <Heading as="h2" size="lg" mb="4">
-            {foodType}
+            {label}
           </Heading>
-
           <VStack spacing="4" align="start">
-            
-          {offerItemsCache[foodType].map((item, index) => (
-  <Box
-    key={index}
-    p="2"
-    borderWidth="3px"
-    borderRadius="xl"
-    w={{ base: '100%', md: '75%', lg: '50%' }}
-    margin="auto"
-    mt="4"
-    boxShadow="md"
-    textAlign="center"
-  >
-    <Text fontWeight="bold" fontSize="xl">
-      {item.foodname}
-    </Text>
-    <Box mt="2">
-      <Text>{item.description}</Text>
-    </Box>
-    <Flex justifyContent="center" alignItems="center" mt="2">
-      <Text alignSelf="flex-start">{item.description ? 'on its own' : ''}</Text>
-      <Box ml="2">£{item.foodprice}</Box>
-    </Flex>
-    <Flex justifyContent="center" alignItems="center" mt="2">
-      {item.imageURL && (
-        <Image
-          src={item.imageURL} // Use imageURL here
-          alt={item.foodname}
-          boxSize="200px"
-          objectFit="cover"
-          onError={(e) => {
-            console.error('Error loading image:', e);
-          }}
-        />
-      )}
-    </Flex>
-    {isSignedIn && (
-      <Flex justifyContent="center" mt="4">
-        <Button colorScheme="red" onClick={() => handleDelete(foodType, item.foodname)}>
-          Delete
-        </Button>
-      </Flex>
-    )}
-  </Box>
-))}
-
+            {(offerItemsCache[type] || []).map((item, index) => (
+              <Box
+                key={index}
+                p="2"
+                borderWidth="3px"
+                borderRadius="xl"
+                w={{ base: '100%', md: '75%', lg: '50%' }}
+                margin="auto"
+                mt="4"
+                boxShadow="md"
+                textAlign="center"
+              >
+                <Text fontWeight="bold" fontSize="xl">
+                  {item.foodname}
+                </Text>
+                <Box mt="2">
+                  <Text>{item.description}</Text>
+                </Box>
+                <Flex justifyContent="center" alignItems="center" mt="2">
+                  <Text alignSelf="flex-start">{item.description ? 'on its own' : ''}</Text>
+                  <Box ml="2">£{item.foodprice}</Box>
+                </Flex>
+                <Flex justifyContent="center" alignItems="center" mt="2">
+                  {item.imageurl && (
+                    <Image
+                      src={item.imageurl}
+                      alt={item.foodname}
+                      boxSize="200px"
+                      objectFit="cover"
+                      onError={(e) => {
+                        console.error('Error loading image:', e);
+                        // Optionally, you can display a placeholder image or fallback content here
+                      }}
+                    />
+                  )}
+                </Flex>
+                {isSignedIn && (
+                  <Flex mt="2" justifyContent="flex-end">
+                    <Button colorScheme="red" onClick={() => handleDelete(type, item.foodname)}>
+                      Delete
+                    </Button>
+                  </Flex>
+                )}
+              </Box>
+            ))}
           </VStack>
-
           {isSignedIn && (
-            <Box w={{ base: '100%', md: '75%', lg: '50%' }} margin="auto" mt="4">
-              <form onSubmit={(e) => handleSubmit(e, foodType)}>
-                <VStack spacing="4" align="stretch">
-                  <FormControl isRequired>
-                    <FormLabel>Name</FormLabel>
-                    <Input
-                      value={formData.name}
-                      onChange={(e) => setFormData({ ...formData, name: e.target.value })}
-                    />
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel>Price</FormLabel>
-                    <Input
-                      type="number"
-                      step="0.01"
-                      value={formData.price}
-                      onChange={(e) => setFormData({ ...formData, price: e.target.value })}
-                    />
-                  </FormControl>
-                  <FormControl isRequired>
-                    <FormLabel>Description</FormLabel>
-                    <Textarea
-                      value={formData.description}
-                      onChange={(e) => setFormData({ ...formData, description: e.target.value })}
-                    />
-                  </FormControl>
-                  <FormControl>
-                    <FormLabel>Image</FormLabel>
-                    <Input type="file" onChange={handleImageUpload} />
-                  </FormControl>
-                  <Button colorScheme="teal" type="submit">
-                    Add Offer
-                  </Button>
-                </VStack>
-              </form>
+            <Box as="form" onSubmit={(e) => handleSubmit(e, type)} mt="4">
+              <FormControl mb="4">
+                <FormLabel>Name:</FormLabel>
+                <Input
+                  type="text"
+                  name="name"
+                  placeholder="Enter item name"
+                  value={formData.name}
+                  onChange={(e) => setFormData({ ...formData, name: e.target.value })}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel>Price:</FormLabel>
+                <Input
+                  type="text"
+                  name="price"
+                  placeholder="Enter item price"
+                  value={formData.price}
+                  onChange={(e) => setFormData({ ...formData, price: e.target.value })}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <FormLabel>Description:</FormLabel>
+                <Textarea
+                  name="description"
+                  placeholder="Enter item description"
+                  value={formData.description}
+                  onChange={(e) => setFormData({ ...formData, description: e.target.value })}
+                  required
+                />
+              </FormControl>
+              <FormControl mb="4">
+                <Input
+                  type="file"
+                  accept="image/*"
+                  onChange={handleImageUpload}
+                />
+              </FormControl>
+              <Button type="submit" colorScheme="orange">
+                Add {label}
+              </Button>
             </Box>
           )}
         </Box>
       ))}
+      <Box
+        as="footer"
+        position="absolute"
+        bottom="0"
+        left="0"
+        width="100%"
+        bg="black"
+        p="4"
+        textAlign="center"
+        borderTop="2px"
+        borderColor="black"
+        color="white"
+      >
+        <Text fontSize={{ base: 'md', md: 'lg' }} fontWeight="bold">
+          UPGRADE TO A MEAL +£1.50
+        </Text>
+      </Box>
     </Container>
   );
 };
-
-
-
-
 
 export default OffersPage;
